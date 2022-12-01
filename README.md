b4784c3dd65aabfb1dd766023ccab2222730b6e2
Andy Desmarais <andydes@amazon.com>
2022-12-01 11:51:05 -0500

    Clarical work

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # advent-of-code-2022
-My problem solution for https://adventofcode.com/2022
+My problem solutions for <https://adventofcode.com/2022>
+
+# Usage
+
+`npm start` and enter which day to solve!
--- day-1/index.ts
+++ day-1/index.ts
@@ -1,5 +1,5 @@
 /**
- * Problem statement:
+ * Problem statement Part 1:
  * The jungle must be too overgrown and difficult to navigate in vehicles or access from the air; the Elves' expedition traditionally goes on foot. As your boats approach land, the Elves begin taking inventory of their supplies. One important consideration is food - in particular, the number of Calories each Elf is carrying (your puzzle input).
 
 The Elves take turns writing down the number of Calories contained by the various meals, snacks, rations, etc. that they've brought with them, one item per line. Each Elf separates their own inventory from the previous Elf's inventory (if any) by a blank line.
@@ -30,6 +30,8 @@ The fifth Elf is carrying one food item with 10000 Calories.
 In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like to know how many Calories are being carried by the Elf carrying the most Calories. In the example above, this is 24000 (carried by the fourth Elf).
 
 Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+
+Part 2: Sum top three highest calorie elves
  */
 
 import { parseInputs } from "./parse-input";
